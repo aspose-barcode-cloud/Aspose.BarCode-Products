2de0b62b65f41fddee58b96d939bf5f5244a2403
Denis Averin <denis.averin@aspose.com>
2025-09-19 20:30:34 +0700

    Follow symlinks

--- scripts/change-urls.bash
+++ scripts/change-urls.bash
@@ -4,5 +4,5 @@ set -euo pipefail
 DST_DIR="$1"
 
 echo "Find and Replace: url: -> url_ignore: in all *_index.md files in ${DST_DIR}"
-find "${DST_DIR}" -type f -name "_index.md" -exec sed -i 's/^url:/url_ignore:/g' {} +
+find -L "${DST_DIR}" -type f -name "_index.md" -exec sed -i 's/^url:/url_ignore:/g' {} +
 echo "URLs were replaced"
