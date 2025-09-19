00d79a8e66367077008edca86874623979c91072
Denis Averin <denis.averin@aspose.com>
2025-09-19 18:37:40 +0700

    Change hugo-server.bash to first build content and then serve

--- scripts/hugo-server.bash
+++ scripts/hugo-server.bash
@@ -1,19 +1,32 @@
 #!/bin/bash
 set -euo pipefail
 
-HUGO_CONTENT_DIR="./barcode"
+HUGO_CONTENT_DIR="content"
 
 HUGO_CONFIG="./submodules/products.aspose.cloud/config-prod.toml"
 HUGO_THEMES_DIR="./submodules/products.aspose.cloud/themes/"
 
+HUGO_BASE_URL="https://aspose.test"
 
 # Go to content directory
 pushd "$(dirname "$0")/.."
 
 # See https://github.com/aspose-cloud/products.aspose.cloud-workflows/blob/main/.github/workflows/barcode-production.yml
+echo "Building site with Hugo..."
+hugo \
+  --baseURL="${HUGO_BASE_URL}" \
+  --cleanDestinationDir \
+  --config "${HUGO_CONFIG}" \
+  --contentDir "${HUGO_CONTENT_DIR}" \
+  --ignoreCache \
+  --themesDir "${HUGO_THEMES_DIR}" \
+  --verbose
+
+echo "Starting Hugo server..."
 hugo server \
-  --baseURL="https://products.aspose.cloud/barcode" \
+  --baseURL="${HUGO_BASE_URL}" \
   --bind "127.0.0.1" \
+  --port 1313 \
   --buildDrafts \
   --buildExpired \
   --buildFuture \
@@ -24,7 +37,6 @@ hugo server \
   --disableFastRender \
   --gc \
   --ignoreCache \
-  --port 1313 \
   --themesDir "${HUGO_THEMES_DIR}" \
   --verbose \
   --watch
